06ff288fc4b4a633987b2f0fea052ded01c4dbe2
Karina de Oliveira Rovani <kkarinarovani@gmail.com>
2023-06-08 15:13:54 -0300

    code improvements

--- src/main/java/com/devsuperior/bds02/controller/CityController.java
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -3,6 +3,9 @@ package com.devsuperior.bds02.controller;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,12 +20,6 @@ public class CityController {
     @Autowired
     private CityService service;
 
-    @GetMapping
-    public ResponseEntity<List<CityDTO>> findAll() {
-        List<CityDTO> list = service.findAll();
-        return ResponseEntity.ok().body(list);
-    }
-
     @PostMapping
     public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
         dto = service.insert(dto);
@@ -30,10 +27,38 @@ public class CityController {
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CityDTO> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<CityDTO> delete(@PathVariable Long id){
-        service.delete(id);
-        return ResponseEntity.noContent().build();
-    }
+	
+	@GetMapping
+	public ResponseEntity<Page<CityDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<CityDTO> list = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CityDTO> findById(@PathVariable Long id){
+		CityDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CityDTO> update(@PathVariable Long id, @RequestBody CityDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 }
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -5,20 +5,21 @@ import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.services.exceptions.DatabaseException;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class CityService {
@@ -33,6 +34,20 @@ public class CityService {
                 .map(x-> new CityDTO(x))
                 .collect(Collectors.toList());
     }
+
+	@Transactional(readOnly = true)
+	public Page<CityDTO> findAllPaged(PageRequest PageRequest){
+		Page<City> list = repository.findAll(PageRequest);
+		
+		return list.map(x -> new CityDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public CityDTO findById(Long id) {
+		Optional<City> obj = repository.findById(id);
+		City entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		return new CityDTO(entity);
+	}
 
     @Transactional
     public CityDTO insert(CityDTO dto) {
@@ -55,4 +70,16 @@ public class CityService {
             throw new DatabaseException("Integrity violation");
         }
     }
+
+	@Transactional
+	public CityDTO update(Long id, CityDTO dto) {
+		try {
+		City entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+		} catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
 }
